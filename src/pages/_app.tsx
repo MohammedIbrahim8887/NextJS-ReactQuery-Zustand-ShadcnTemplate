03e9753c33101ca60ad383a07b39bee7236b5e4f
Mohammed Ibrahim <agentmame@gmail.com>
2023-10-07 19:08:54 +0300

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Toaster } from "@/components/ui/toaster";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [queryClient] = useState(() => new QueryClient());
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <Toaster />
+    </QueryClientProvider>
+  );
 }
